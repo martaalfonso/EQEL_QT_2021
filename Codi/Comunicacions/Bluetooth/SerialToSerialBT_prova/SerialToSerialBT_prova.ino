6067a55a016725c0b534851fa4ec508d45fc75bf
marta <mardaro9@gmail.com>
2021-10-28 16:23:01 +0200

    MQTT i HTTP funcionen. El GPS es comunica però no desgranem bé la informació. Afegits altres documents.

---
+++ Codi/Comunicacions/Bluetooth/SerialToSerialBT_prova/SerialToSerialBT_prova.ino
@@ -0,0 +1,34 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second  
+}
---
+++ Codi/Comunicacions/HTTP/esp32-http-get-post-arduino/esp32-http-get-post-arduino.ino
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "TP-Link_D128";
+const char* password = "46104809";
+
+//Your Domain name with URL path or IP address with path
+String serverName = "http://192.168.1.132:1880/update-sensor";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+// Set your Static IP address
+IPAddress local_IP(127, 0, 0, 2);
+// Set your Gateway IP address
+IPAddress gateway(172, 26, 32, 1); //fet ip route a bash de wsl 2
+
+IPAddress subnet(255, 0, 0, 1);
+
+void setup() {
+  Serial.begin(115200);
+   
+  // Configures static IP address
+//  if (!WiFi.config(local_IP, gateway, subnet)) {
+//    Serial.println("STA Failed to configure");
+//  }
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+      String serverPath = serverName + "?temperature=24.37";
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
